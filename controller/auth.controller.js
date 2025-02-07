5f56529a9542a8b41647a8875c593745d19e1379
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-07 12:36:09 +0330

    Added Register Section

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,15 +1,26 @@
+import bcrypt from "bcryptjs";
 import User from "../model/user.model.js";
 
+// ============== Register ===============
 export const register = async (req, res, next) => {
   try {
     const { fullName, email, password } = req.body;
-    const user = await User.create({ fullName, email, password });
-
-    res.send(user);
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "Email already in use" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({ message: "User Registered Successfully", user });
   } catch (error) {
     next(error);
   }
 };
+
+// ============== Login ===============
 export const login = (req, res, next) => {
   res.send("Login System");
 };
