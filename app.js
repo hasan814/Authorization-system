bb72eb2f41c9cdb2f45bd61b1269d3f34f1cc5e4
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-07 10:15:03 +0330

    Finish Folder Structures

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+import { ErrorHandler, NotFoundError } from "./utils/error-Handling.js";
+
 import express from "express";
-import { ErrorHandler, NotFoundError } from "./utils/error-Handling";
+import router from "./router/index.router.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use();
+app.use(router);
 app.use(NotFoundError);
 app.use(ErrorHandler);
+
+app.listen(3000, () => {
+  console.log("Server run on port:3000");
+});
