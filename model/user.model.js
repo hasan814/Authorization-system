7eaaebbb68167b881f733ccdf9cfb3acbc7beec5
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-07 10:02:40 +0330

    Added UserModel

---
+++ model/user.model.js
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, "Invalid email format"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("User", userSchema);
+export default User;
