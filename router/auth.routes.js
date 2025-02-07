1d15484ce6958315e95fcb4ecb1e60ea7c6a68ac
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-07 13:10:39 +0330

    Added express-validation

--- router/auth.routes.js
+++ router/auth.routes.js
@@ -1,9 +1,13 @@
 import { login, register } from "../controller/auth.controller.js";
 import { Router } from "express";
+import {
+  validateLogin,
+  validateRegister,
+} from "../validators/authValidator.js";
 
 const authRouter = Router();
 
-authRouter.post("/login", login);
-authRouter.post("/register", register);
+authRouter.post("/login", validateLogin, login);
+authRouter.post("/register", validateRegister, register);
 
 export default authRouter;
---
+++ validators/authValidator.js
@@ -0,0 +1,30 @@
+import { body, validationResult } from "express-validator";
+
+// Validation rules for registration
+export const validateRegister = [
+  body("fullName").notEmpty().withMessage("Full Name is required"),
+  body("email").isEmail().withMessage("Invalid email format"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+// Validation rules for login
+export const validateLogin = [
+  body("email").isEmail().withMessage("Invalid email format"),
+  body("password").notEmpty().withMessage("Password is required"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
